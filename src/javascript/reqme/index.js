d8ea972d88a0542d9f4bc2849debfde23592d8b6
Alex Sherman <me.alexsherman@gmail.com>
2019-05-24 22:12:48 -0400

    basic style n stuff

--- src/javascript/reqme/index.js
+++ src/javascript/reqme/index.js
@@ -1,10 +1,10 @@
 function SummonerSquare(props) {
     const champ = props.champion;
     return (
-        <React.Fragment>
-            <h1>{champ}</h1>
+        <div className="summoner-square">
             <ChampSquare champion={champ} />
-        </React.Fragment>
+            <div className="champion-name">{champ}</div>
+        </div>
     )
 }
 
@@ -12,17 +12,27 @@ function ChampSquare(props) {
     const name = props.champion;
     const src = "http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/" + name + ".png";
     return (
-        <img src={src} />
+        <img className="champion-square" src={src} />
+    )
+}
+
+function TeamLabel(props) {
+    return (
+        <div className="team-label">
+            {props.label}
+        </div>
     )
 }
 
 function Team(props) {
     const champs = props.teamdata.champs;
+    const label = props.label;
     const summonerSquares = champs.map((champ) => 
         <SummonerSquare key={champ} champion={champ} />
     );
     return (
         <div className="team-container">
+            <TeamLabel label={label} />
             {summonerSquares}
         </div>
     );
@@ -35,16 +45,13 @@ function Reqs(props) {
     const reqs = props.resp;
     const indivReqs = reqs.map((champ) => 
         <React.Fragment>
-            <li key={champ}>{champ}</li>
-            <ChampSquare champion={champ} />
+            <SummonerSquare key={champ} champion={champ} />
         </React.Fragment>
     );
     return (
         <div className="req-container">
-            We recommend:
-            <ul>
-                {indivReqs}
-            </ul>
+            The best champs for your team are:
+            {indivReqs}            
         </div>
     )
 }
@@ -68,8 +75,7 @@ class ChampionSelect extends React.Component {
     }
 
     componentDidMount() {
-            //this.getReqs();
-            // open websocket, ping it if possible
+        // todo - move to separate file, have retry every 10s or manual button
         let tm = undefined;
         let webSocket = new WebSocket('ws://localhost:5000');
         webSocket.onmessage = event => {
@@ -90,6 +96,7 @@ class ChampionSelect extends React.Component {
     }
 
     getReqs() {
+        // todo - move to separate file to handle all recommendation API call logic
         const baseUrl = 'http://localhost:8000/globalreq';
         let params = '?';
         if (this.state.sameTeam.champs.length > 0) {
@@ -125,20 +132,12 @@ class ChampionSelect extends React.Component {
         })
     }
 
-    addChampToTeam() {
-        this.setState(prevState => ({
-                 team: {
-                    champs: [...prevState.team.champs, "bew cgano"]
-                 }
-        }));
-    }
-
     render() {
         return (
                 <div id="app-container">
-                    <Team teamdata={this.state.sameTeam} />
+                    <Team teamdata={this.state.sameTeam} label="Your Team" />
                     <Reqs resp={this.state.req} />
-                    <Team teamdata={this.state.oppTeam} />
+                    <Team teamdata={this.state.oppTeam} label="Enemy Team"/>
                 </div>
             )
         }
